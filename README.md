2c70c2feb163734940614d05c1256397b1a42c2b
rosselgalarzarafael <rosselgalarzarafael@gmail.com>
2024-10-01 00:01:36 -0500

    desarrollo del login

---
+++ README.md
@@ -0,0 +1,48 @@
+
+# MediCita - Plataforma Web para la Administración de Citas Médicas
+
+![Vue.js](https://vuejs.org/images/logo.png) ![Vuetify](https://cdn.vuetifyjs.com/images/logos/vuetify-logo-light.svg)
+
+**MediCita** es una plataforma web diseñada para la administración de citas médicas en un hospital. Desarrollada con **Vue.js** y **Vuetify**, esta aplicación proporciona una interfaz moderna y eficiente para gestionar citas, pacientes, médicos, internamientos y visitas médicas.
+
+
+## Instalar todas las dependencias
+```
+npm install
+```
+
+### Comando para arrancar el proyecto de manera local
+```
+vue serve 
+```
+
+### Compando para generar el dist y desplegarlo en producción 
+```
+npm run build
+```
+
+
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+
+# VISTAS DEL PROYECTO 
+### vista login
+
+<img src="./src/assets/README/login.png" alt="rafadev" style="width: 100%; height: auto;">
+
+### vista pacientes
+<img src="./src/assets/README/pacientes.png" alt="rafadev" style="width: 100%; height: auto;">
+
+### vista Medicos
+<img src="./src/assets/README/medicos.png" alt="rafadev" style="width: 100%; height: auto;">
+
+### vista Internamientos
+<img src="./src/assets/README/internamientos.png" alt="rafadev" style="width: 100%; height: auto;">
+
+### vista Visitas medicas
+<img src="./src/assets/README/visitasMedicas.png" alt="rafadev" style="width: 100%; height: auto;">
+
+
---
+++ src/router/index.js
@@ -0,0 +1,75 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import store from '@/store';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            meta: {
+                title: "Home",
+                requiresAuth: true
+            },
+            path: "/",
+            component: () => import("@/layouts/MasterLayout.vue"),
+            children: [
+              
+             
+
+                // {
+                //     name: "visitas",
+                //     path: "visitas",
+                //     component: () => import("@/views/VisitasView.vue"),
+                // }
+                
+                
+       
+            ]
+        },
+        {
+            meta: {
+                title: "Login"
+            },
+            path: "/login",
+            name: "login",
+            component: () => import("@/views/LoginView.vue")
+        },
+      
+        {
+            path: '/:pathMatch(.*)*', // Cualquier ruta no especificada
+            redirect: '/login' // Redirigir a login
+        }
+    ]
+})
+
+// Navigation Guard  
+router.beforeEach((to, from, next) => {
+    const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+    if (requiresAuth && !store.state.isAuthenticated) {
+      
+        next({ name: 'login' });
+    } else if (to.name === 'login' && store.state.isAuthenticated) {
+        // Si el usuario ya está autenticado y trata de acceder al login, redirigir según su rol
+        if (store.state.role === "usuario") {
+            next({ name: '' });
+        } else if (store.state.role === "Doctor") {
+            next({ name: '' });
+        } else {
+            next();
+        }
+    } else if (to.path === '/' && store.state.isAuthenticated) {
+        // Si el usuario está autenticado y accede a la raíz, redirigir según su rol
+        if (store.state.role === "usuario") {
+            next({ name: '' });
+        } else if (store.state.role === "Doctor") {
+            next({ name: '' });
+        } else {
+            next();
+        }
+    } else {
+        // Si no hay ninguna condición que lo bloquee, permitir el acceso a la ruta solicitada
+        next();
+    }
+});
+
+export default router;
